ce5dabb25cd2ed0719f1f6328f4c51bc75c76842
seriksso <seriksso@hamilton.edu>
2025-04-11 15:21:50 -0400

    calculated tile in VRAM

--- src/collision.asm
+++ src/collision.asm
@@ -27,3 +27,43 @@ endm
         ; add 8 to x coordinate and check that tile
     ; if moving left:
         ; dec and 
+; \1 is global x_coordinate
+; \2 is global y_coordinate
+macro find_overlapping_tile_ID
+    ; calculate what tile the sprite is on
+    ld a, \1
+    srl a
+    srl a
+    srl a
+    sla a
+    sla a
+    sla a
+    ld b, a
+
+    ld a, \2
+    srl a
+    srl a0
+    srl a
+    sla a
+    sla a
+    sla a
+    ld c, a
+
+    ; multiply y * 32
+    ld hl, c
+    sla hl
+    sla hl
+    sla hl
+    sla hl
+    sla hl
+    
+    ; add (y * 32) + x
+    add hl, b
+    
+    add hl, $8000
+
+    
+
+    ; check vram for tile ID
+    ; tilemap + calculated number    
+endm
